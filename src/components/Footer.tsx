c3a5e356850ccb610c823ef481dfa5e197efc125
biswa-ganguly <biswaganguly16@gmail.com>
2024-06-02 21:29:34 +0530

    final commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+
+function Footer() {
+  return (
+    <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
+      <p className="text-base font-bold text-white">@2023 EpicAnimeVault <><br /></> Made with love from Biswa</p>
+
+      <Image
+        src="./logo.svg"
+        alt="logo"
+        width={50}
+        height={44}
+        className="object-contain "
+      />
+      <div className="flex items-center gap-6">
+        <Image
+          src="./instagram.svg"
+          alt="logo"
+          width={19}
+          height={19}
+          className="object-contain"
+        />
+        <Image
+          src="./twitter.svg"
+          alt="logo"
+          width={19}
+          height={19}
+          className="object-contain"
+        />
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/MotionDiv.tsx
@@ -0,0 +1,6 @@
+"use client"
+
+import {delay, motion} from 'framer-motion'
+
+export const MotionDiv = motion.div
+
